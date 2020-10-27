ed6abdcd9314abe4063b80303bcb608ace37b80e
ycs <499427445@qq.com>
2020-10-27 16:35:43 +0800

    pdf预览和vue跨域http://mozilla.github.io/pdf.js/getting_started/#download

--- src/view/form/draggableTest.vue
+++ src/view/form/draggableTest.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="root">
+    <!--pdf预览地址-->
+    <div class="show-certificat-img">
+      <a :href="'../../../static/js/pdf/web/viewer.html?file=' + url" target="_blank">哈哈哈</a>
+    </div>
 
     <draggable
       :data="'希望drop组件接收到的数据'"
       @dragstart="onSourceDragStart"
       @dragsend="onSourceDragEnd">
       <div class="drag-content">拖动我</div>
-
     </draggable>
-    <el-button v-drag="{set: set}" style="position: absolute" type="primary">拖动我</el-button>
-
     <droppable
       class="drop-box"
       @dragstart="onDragStart"
@@ -18,8 +19,10 @@
       @dragenter="onDragEnter"
       @dragover="onDragOver"
       @dragleave="onDragLeave">
-      <div style="width:500px; height: 500px;background-color: blue">
-      <el-button v-drag="{set: set}" v-for="(item,i) in list" :key="item" :style="{'position': 'absolute', 'top': i*60 + 50 +'px' }" type="primary">拖动我{{item}}</el-button>
+      <div style="position:relative; width:500px; height: 500px;background-color: blue">
+        <div  v-drag="{set: set}" v-for="(item,i) in list" :key="item" :style="{'position': 'absolute', 'top': i*60 + 50 +'px', 'left': i*10 + 30 + 'px' }">
+          <el-button type="primary">拖动我{{item}}</el-button>
+        </div>
       </div>
 
 
@@ -38,6 +41,7 @@
   export default {
     data() {
       return {
+        url: encodeURIComponent('http://localhost:8080/api/file/view?code=wKhYdF8Puz-AA6YfAAG-AA0DWH4941'),
         list: [88,2,3]
       }
     },
@@ -57,18 +61,18 @@
           document.onmouseup = function(e) {
             // console.log(this.data.arr)
             // e.preventDefault()
-            // if (el.offsetLeft < 0) {
-            //   el.style.left = 0
-            // }
-            // if (el.offsetLeft > 515) {
-            //   el.style.left = '515px'
-            // }
-            // if (el.offsetTop < 0) {
-            //   el.style.top = 0
-            // }
-            // if (el.offsetTop > 794) {
-            //   el.style.top = '794px'
-            // }
+            if (el.offsetLeft < 0) {
+              el.style.left = 0
+            }
+            if (el.offsetLeft > 410) {
+              el.style.left = '410px'
+            }
+            if (el.offsetTop < 0) {
+              el.style.top = 0
+            }
+            if (el.offsetTop > 460) {
+              el.style.top = '460px'
+            }
             document.onmousemove = document.onmouseup = null
             bindings.value.set(el.style.left, el.style.top)
           }
@@ -93,14 +97,13 @@
         console.log('监听到拖动结束', params)
       },
       onDrop (params) {
-        this.list.push(this.list.length + 1)
+        // this.list.push(this.list.length + 1)
         console.log('监听到被拖动元素放下', params)
       },
       onDragEnter (params) {
         console.log('监听到被拖动元素进入当前范围', params)
       },
       onDragOver (params) {
-
         //console.log('监听到被拖动元素在上方移动, 这个调用调多次就不打印了')
       },
       onDragLeave (params) {
